5321af2bf6d6d0bfbe8cad63f617949bb4f95407
Robin Krahl <robin.krahl@ireas.org>
2021-05-13 13:12:18 +0200

    Add owned versions for list_readers, get_attribute and status2
    Currently, the user has to manually allocate buffers for these
    functions, typically calling the associated _len function to determine
    the size of the buffers.  This is handy if the user wants to manually
    control allocations, but it is unnecessary complicated and error-prone
    in less restricted environments.
    
    Therefore, this patch introduces owned variants of these functions that
    do not expect the buffer as an argument but instead determine the
    required buffer size, allocate a buffer and return an owned version of
    the data.
    
    Fixes #17.

---
+++ pcsc/examples/list-readers-owned.rs
@@ -0,0 +1,17 @@
+// Example of how to enumerate connected card readers.
+
+extern crate pcsc;
+
+use pcsc::*;
+
+fn main() {
+    // Get a context.
+    let ctx = Context::establish(Scope::User).expect("failed to establish context");
+
+    // Instead of manually allocating the buffer for the reader names, we let pcsc take care of
+    // that
+    let names = ctx.list_readers_owned().expect("failed to list readers");
+    for name in names {
+        println!("{:?}", name);
+    }
+}
